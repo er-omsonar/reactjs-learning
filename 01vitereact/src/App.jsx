0fea35d0465c104bda0cf973374197084b00e4ed
er-omsonar <er.omsonar@gmail.com>
2025-09-27 17:42:01 +0530

    install vite react and create first project in react

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+
+  return (
+    <>
+     <h1>chai aur code | om sonar | This is my first project in react in that i learned that how to install react app and file structure of it.</h1>
+    </>
+  )
+}
+
+export default App
